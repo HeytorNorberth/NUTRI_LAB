1d9a61dd80fc7e7cae3730c8f8b55cfa8d6ede7d
Heytor Norberth <heytornorberth2@gmail.com>
2022-08-03 16:47:36 -0300

    Add files via upload

---
+++ autenticacao/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        return HttpResponse(confirmar_senha)
+
+
+def logar(request):
+    pass
